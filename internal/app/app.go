36c850aeb84eecf0a335ecd29bbb6cc7f20d8f69
Rafael Caixeta <caixetadev@gmail.com>
2023-09-29 19:37:53 +0200

    feat: add app

---
+++ internal/app/app.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/Caixetadev/snippet/config"
+	"github.com/Caixetadev/snippet/internal/routes"
+	"github.com/Caixetadev/snippet/internal/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Run(cfg *config.Config, db *pgxpool.Pool, router *gin.Engine, validation validation.Validator) {
+	publicRouter := router.Group("")
+
+	routes.NewLoginRouter(cfg, db, publicRouter, validation)
+}
