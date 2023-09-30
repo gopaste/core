eb4f2fea6cfee8725b142c95c536a050bc3d9458
Rafael Caixeta <caixetadev@gmail.com>
2023-10-01 00:23:22 +0200

    feat: add mocks

---
+++ internal/mocks/mocks.go
@@ -0,0 +1,40 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/Caixetadev/snippet/internal/core/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type UserRepository struct {
+	mock.Mock
+}
+
+func (m *UserRepository) Create(ctx context.Context, user *domain.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (m *UserRepository) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	return nil, nil
+}
+
+func (m *UserRepository) UserExistsByEmail(ctx context.Context, email string) (bool, error) {
+	args := m.Called(ctx, email)
+	return args.Bool(0), args.Error(1)
+}
+
+type PasswordHasher struct {
+	mock.Mock
+}
+
+func (m *PasswordHasher) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
+	args := m.Called(password, cost)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (m *PasswordHasher) CompareHashAndPassword(hashedPassword, password []byte) error {
+	args := m.Called(hashedPassword, password)
+	return args.Error(1)
+}
