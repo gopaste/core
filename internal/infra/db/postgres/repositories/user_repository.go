24957f8396069b18c44eb2f25d27f5d8de30b1d8
Rafael Caixeta <caixetadev@gmail.com>
2023-09-30 08:34:55 +0200

    feat: add user repository

--- internal/infra/db/postgres/repositories/user_repository.go
+++ internal/infra/db/postgres/repositories/user_repository.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+	"database/sql"
+
 	"github.com/Caixetadev/snippet/internal/core/domain"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -13,6 +16,38 @@ func NewUserRepository(db *pgxpool.Pool) *userRepository {
 	return &userRepository{db: db}
 }
 
-func (ur *userRepository) GetUserByEmail(email string) (*domain.User, error) {
-	return nil, nil
+func (ur *userRepository) Create(ctx context.Context, user *domain.User) error {
+	_, err := ur.db.Exec(ctx, "INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)", user.ID, user.Name, user.Email, user.Password)
+	return err
+}
+
+func (ur *userRepository) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	line, err := ur.db.Query(ctx, "SELECT id, name, password FROM users WHERE email = $1", email)
+	if err != nil {
+		return nil, err
+	}
+
+	defer line.Close()
+
+	var user domain.User
+
+	if line.Next() {
+		if err = line.Scan(&user.ID, &user.Name, &user.Password); err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, sql.ErrNoRows
+	}
+
+	return &user, nil
+}
+
+func (ur *userRepository) UserExistsByEmail(ctx context.Context, email string) (bool, error) {
+	var exists bool
+	err := ur.db.QueryRow(ctx, "SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)", email).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+
+	return exists, nil
 }
