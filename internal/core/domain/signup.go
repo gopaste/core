1ea365554b0052034ad595d321624abd277aacfb
Rafael Caixeta <caixetadev@gmail.com>
2023-12-06 17:34:09 +0100

    feat: add signup domain

--- internal/core/domain/signup.go
+++ internal/core/domain/signup.go
@@ -1,7 +1,5 @@
 package domain
 
-// type SignupRequest struct {
-// 	Name     string `json:"name" validate:"required"`
-// 	Email    string `json:"email" validate:"required,email"`
-// 	Password string `json:"password" validate:"required"`
-// }
+type SignupResponse struct {
+	AccessToken string `json:"acessToken"`
+}
