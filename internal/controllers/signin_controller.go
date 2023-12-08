7dc79fdb7f23a7edbfa007ed8a2cb46b41a24ceb
Rafael Caixeta <caixetadev@gmail.com>
2023-12-08 16:15:19 +0100

    feat: add controller to sign in

--- internal/controllers/signin_controller.go
+++ internal/controllers/signin_controller.go
@@ -1,16 +1,49 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/Caixetadev/snippet/config"
 	"github.com/Caixetadev/snippet/internal/core/domain"
+	"github.com/Caixetadev/snippet/internal/core/error"
 	"github.com/gin-gonic/gin"
 )
 
 type SigninController struct {
-	UserService domain.UserRepository
+	UserService domain.SignupService
 	Env         *config.Config
 }
 
-func (lc *SigninController) Signin(ctx *gin.Context) {
-	lc.UserService.GetUserByEmail(ctx, "")
+func (sc *SigninController) Signin(ctx *gin.Context) {
+	var payload domain.SigninRequest
+
+	err := ctx.ShouldBindJSON(&payload)
+	if err != nil {
+		ctx.Error(error.BadRequest)
+		return
+	}
+
+	user, err := sc.UserService.GetUserByEmail(ctx, payload.Email)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	err = sc.UserService.CompareHashAndPassword(user.Password, payload.Password)
+	if err != nil {
+		ctx.Error(error.Unauthorized)
+		return
+	}
+
+	token, err := sc.UserService.CreateAccessToken(user, sc.Env.AccessTokenSecret, sc.Env.AccessTokenExpiryHour)
+	if err != nil {
+		ctx.Error(error.BadRequest)
+		return
+	}
+
+	signinResponse := domain.SigninResponse{
+		AccessToken: token,
+	}
+
+	ctx.JSON(http.StatusOK, signinResponse)
 }
