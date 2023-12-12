451dbb9f2f927e186fb40e32c163e657b27ca534
Rafael Caixeta <caixetadev@gmail.com>
2023-12-12 13:59:08 +0100

    feat: add function to create an user

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -13,16 +13,26 @@ type User struct {
 	Password string    `json:"password"     validate:"required"       binding:"required"`
 }
 
+func NewUser(name, email, password string) *User {
+	uuidGenerator := UUIDGeneratorImpl{}
+
+	return &User{
+		ID:       uuidGenerator.Generate(),
+		Name:     name,
+		Email:    email,
+		Password: password,
+	}
+}
+
 type UserRepository interface {
 	GetUserByEmail(ctx context.Context, email string) (*User, error)
-	Create(ctx context.Context, user *User) error // Create(user *User) error
-
+	Create(ctx context.Context, user *User) error
 	UserExistsByEmail(ctx context.Context, email string) (bool, error)
 }
 
 type SignupService interface {
 	GetUserByEmail(ctx context.Context, email string) (*User, error)
-	Create(ctx context.Context, user *User) error // Create(user *User) error
+	Create(ctx context.Context, user *User) (*User, error)
 	UserExistsByEmail(ctx context.Context, email string) (bool, error)
 	CreateAccessToken(user *User, secret string, expiry int) (string, error)
 	CompareHashAndPassword(passwordInDatabase, passwordRequest string) error
