18ee75493564a659e27e93e56d18d6c66f9ffde1
Rafael Caixeta <caixetadev@gmail.com>
2023-12-12 14:36:46 +0100

    feat: creating middleware for the post creation route

---
+++ internal/middleware/auth.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Caixetadev/snippet/internal/token"
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func AuthPostMiddleware(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authorizationHeader := c.GetHeader("Authorization")
+
+		parts := strings.Fields(authorizationHeader)
+		if len(parts) != 2 {
+			c.Set("x-user-id", nil)
+			c.Next()
+			return
+		}
+
+		tokenString := parts[1]
+
+		authorized, err := token.IsAuthorized(tokenString, secret)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		if authorized {
+			userID, err := token.ExtractIDFromToken(tokenString, secret)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, ErrorResponse{Message: err.Error()})
+				c.Abort()
+				return
+			}
+
+			c.Set("x-user-id", userID)
+			c.Next()
+			return
+		}
+	}
+}
