1aab94e32fe582813618360d699c45bf08a50542
Rafael Caixeta <caixetadev@gmail.com>
2023-12-12 13:58:57 +0100

    feat: create entity for uuid

---
+++ internal/core/domain/uuid.go
@@ -0,0 +1,13 @@
+package domain
+
+import "github.com/google/uuid"
+
+type UUIDGenerator interface {
+	Generate() string
+}
+
+type UUIDGeneratorImpl struct{}
+
+func (g UUIDGeneratorImpl) Generate() uuid.UUID {
+	return uuid.New()
+}
