a815e7cdcd85b3fd678d8a08e2d1792d3fee8838
Rafael Caixeta <caixetadev@gmail.com>
2023-12-23 23:02:39 +0100

    feat: add new variables env

--- config/config.go
+++ config/config.go
@@ -11,6 +11,10 @@ type Config struct {
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	AWSSecretKey         string        `mapstructure:"AWS_SECRET_KEY"`
+	AWSAccessKey         string        `mapstructure:"AWS_ACCESS_KEY"`
+	AWSRegion            string        `mapstructure:"AWS_REGION"`
+	AWSSenderEmail       string        `mapstructure:"AWS_SENDER_EMAIL"`
 }
 
 func NewConfig(path string) (config *Config, err error) {
