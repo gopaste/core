c68f5cb855102121f638eccd7a97dda1d51a6efe
Rafael Caixeta <caixetadev@gmail.com>
2023-12-26 21:45:42 +0100

    refactor: change column name

--- internal/infra/db/postgres/migrations/000002_create_posts_table.up.sql
+++ internal/infra/db/postgres/migrations/000002_create_posts_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS public.posts (
-    post_id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
     user_id UUID REFERENCES public.users(id) ,
     title varchar(255) NOT NULL,
     content TEXT NOT NULL,
