2911ef2a2e9a5838822c3c7c6694003824754373
Rafael Caixeta <caixetadev@gmail.com>
2024-02-04 13:14:38 +0100

    feat: delete post when expired or after being read

--- internal/entity/post.go
+++ internal/entity/post.go
@@ -15,25 +15,29 @@ const (
 )
 
 type PostInput struct {
-	ID          string     `json:"id"`
-	UserID      *string    `json:"-"`
-	Title       string     `json:"title" validate:"required" binding:"required"`
-	Content     string     `json:"content,omitempty" validate:"required" binding:"required"`
-	CreatedAt   time.Time  `json:"created_at"`
-	Password    string     `json:"password,omitempty"`
-	HasPassword bool       `json:"has_password"`
-	Visibility  Visibility `json:"visibility" validate:"omitempty,oneof=private public unlisted"`
+	ID              string     `json:"id"`
+	UserID          *string    `json:"-"`
+	Title           string     `json:"title" validate:"required" binding:"required"`
+	Content         string     `json:"content,omitempty" validate:"required" binding:"required"`
+	CreatedAt       time.Time  `json:"created_at"`
+	ExpirationAt    time.Time  `json:"expiration_at,omitempty"`
+	Password        string     `json:"password,omitempty"`
+	HasPassword     bool       `json:"has_password"`
+	Visibility      Visibility `json:"visibility" validate:"omitempty,oneof=private public unlisted"`
+	DeleteAfterView bool       `json:"delete_after_view"`
 }
 
 type PostOutput struct {
-	ID          string     `json:"id"`
-	UserID      *string    `json:"user_id"`
-	Title       string     `json:"title" validate:"required" binding:"required"`
-	Content     string     `json:"content,omitempty" validate:"required" binding:"required"`
-	CreatedAt   time.Time  `json:"created_at"`
-	Password    string     `json:"-"`
-	HasPassword bool       `json:"has_password"`
-	Visibility  Visibility `json:"visibility,omitempty"`
+	ID              string     `json:"id"`
+	UserID          *string    `json:"user_id"`
+	Title           string     `json:"title" validate:"required" binding:"required"`
+	Content         string     `json:"content,omitempty" validate:"required" binding:"required"`
+	CreatedAt       time.Time  `json:"created_at"`
+	ExpirationAt    time.Time  `json:"expiration_at"`
+	Password        string     `json:"-"`
+	HasPassword     bool       `json:"has_password"`
+	Visibility      Visibility `json:"visibility,omitempty"`
+	DeleteAfterView bool       `json:"delete_after_view"`
 }
 
 type PostUpdateInput struct {
@@ -53,14 +57,16 @@ type PaginationInfo struct {
 	Count int     `json:"count"`
 }
 
-func NewPost(userID *string, title string, content string, password string, hasPassword bool, visibility Visibility) *PostInput {
+func NewPost(userID *string, title string, content string, password string, hasPassword bool, visibility Visibility, expirationAt time.Time, deleteAfterView bool) *PostInput {
 	return &PostInput{
-		ID:          utils.GenerateRandomString(8),
-		UserID:      userID,
-		Title:       title,
-		Content:     content,
-		Password:    password,
-		HasPassword: hasPassword,
-		Visibility:  visibility,
+		ID:              utils.GenerateRandomString(8),
+		UserID:          userID,
+		Title:           title,
+		Content:         content,
+		Password:        password,
+		HasPassword:     hasPassword,
+		Visibility:      visibility,
+		ExpirationAt:    expirationAt,
+		DeleteAfterView: deleteAfterView,
 	}
 }
